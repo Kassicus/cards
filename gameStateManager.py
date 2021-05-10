2dacb36a28507863d288fff6f4a5130a34fbe336
Kassicus <kason.suchow@gmail.com>
2021-05-09 23:08:39 -0600

    Board changes and prep for Game States
    Added text icons to the board that will indcate which game state is 
    currently active, and prepared the gameStateManager.py file to implement 
    this behavior.

---
+++ gameStateManager.py
@@ -0,0 +1,13 @@
+gameStates = ['draw', 'play', 'fight', 'end', 'pass']
+
+gameState = gameStates[0]
+
+class GameState(object):
+    pass
+
+def changeGameState(state):
+    pass
+
+def getGameState():
+    #return(currentGameState)
+    pass
