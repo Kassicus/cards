18d0d1ee706a91544c74c52e68c0b3040dddb5bd
Kassicus <kason.suchow@gmail.com>
2021-05-10 09:32:38 -0600

    Fully reworked card classes.
    There is now a superclass and it holds all the functions. Each card type 
    will now be its own class so that creating instances of the cars is much 
    nicer.
    
    The cards.py file will grow very large because of this, there may be a 
    point where each card gets its own .py file... Although that idea doesnt 
    sound very nice at the moment.

--- cards.py
+++ cards.py
@@ -3,35 +3,32 @@
 import pygame
 import data
 
-class Card:
-    def __init__(self, x, y, face, color, name, description, attack, defense, cost, type):
-        self.x = x
-        self.y = y
+class BaseCard(object):
+    def __init__(self):
+        self.x = -500
+        self.y = -500
 
         self.pos = (self.x, self.y)
 
-        self.color = color
+        self.color = None
 
-        self.name = name
-        self.description = description
+        self.name = ''
+        self.description = ''
 
-        self.attack = attack
-        self.defense = defense
-        self.cost = cost
+        self.attack = 0
+        self.defense = 0
+        self.cost = 0
 
-        self.type = type
+        self.type = ''
 
         self.remove = False
-
         self.hovered = False
-
         self.faceUp = False
 
-        self.face = pygame.transform.scale(pygame.image.load(face), (80, 112))
+        self.face = None
         self.back = pygame.transform.scale(pygame.image.load('assets/cards/back.png'), (80, 112))
 
         self.image = self.back
-
 
     def draw(self, surface):
         surface.blit(self.image, self.pos)
@@ -45,22 +42,7 @@ class Card:
         else:
             self.image = self.back
 
-        if self.type == 'mana':
-            if self.faceUp:
-                if self.hovered:
-                    for event in data.events:
-                        if event.type == pygame.MOUSEBUTTONDOWN:
-                            if data.playerOneMP >= self.cost:
-                                data.playerOneMP -= self.cost
-
-                                if self.color == 'red':
-                                    data.playerOneRedMana += 3
-                                elif self.color == 'green':
-                                    data.playerOneGreenMana += 3
-                                elif self.color == 'blue':
-                                    data.playerOneBlueMana += 3
-
-                                self.remove = True
+        self.checkClicked()
 
     def checkHover(self):
         pos = pygame.mouse.get_pos()
@@ -73,11 +55,75 @@ class Card:
         else:
             self.hovered = False
 
+    def checkClicked(self):
+        for event in data.events:
+            if self.type == 'mana' and self.faceUp and self.hovered and event.type == pygame.MOUSEBUTTONDOWN:
+                if data.playerOneMP >= self.cost:
+                    data.playerOneMP -= self.cost
 
+                    if self.color == 'red':
+                        data.playerOneRedMana += 3
+                    elif self.color == 'green':
+                        data.playerOneGreenMana += 3
+                    elif self.color == 'blue':
+                        data.playerOneBlueMana += 3
+
+                    self.remove = True
+
+class RedMana(BaseCard):
+    def __init__(self):
+        super().__init__()
+
+        self.color = 'red'
+
+        self.name = 'Red Mana'
+        self.description = 'Adds 3 Red Mana'
+
+        self.attack = 0
+        self.defense = 0
+        self.cost = 1
+
+        self.type = 'mana'
+
+        self.face = pygame.transform.scale(pygame.image.load('assets/cards/red/mana.png'), (80, 112))
+
+class BlueMana(BaseCard):
+    def __init__(self):
+        super().__init__()
+
+        self.color = 'blue'
+
+        self.name = 'Blue Mana'
+        self.description = 'Adds 3 Blue Mana'
+
+        self.attack = 0
+        self.defense = 0
+        self.cost = 1
+
+        self.type = 'mana'
+
+        self.face = pygame.transform.scale(pygame.image.load('assets/cards/blue/mana.png'), (80, 112))
+
+class GreenMana(BaseCard):
+    def __init__(self):
+        super().__init__()
+
+        self.color = 'green'
+
+        self.name = 'Green Mana'
+        self.description = 'Adds 3 Green Mana'
+
+        self.attack = 0
+        self.defense = 0
+        self.cost = 1
+
+        self.type = 'mana'
+
+        self.face = pygame.transform.scale(pygame.image.load('assets/cards/green/mana.png'), (80, 112))
 
 library = {
-'001': Card(-500, -500, 'assets/cards/green/mana.png', 'green', 'test', 'test', 0, 1, 1, 'mana'),
-'002': Card(-500, -500, 'assets/cards/blue/mana.png', 'blue', 'test', 'test', 0, 1, 1, 'mana'),
-'003': Card(-500, -500, 'assets/cards/red/mana.png', 'red', 'test', 'test', 0, 1, 1, 'mana'),
-'004': Card(-500, -500, 'assets/cards/red/mana.png', 'red', 'test', 'test', 0, 1, 1, 'mana'),
+'001': RedMana(),
+'002': BlueMana(),
+'003': GreenMana(),
+'004': RedMana(),
 }
